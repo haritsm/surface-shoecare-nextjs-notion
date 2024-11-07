db9cbb8d8fc0328de2cb72f2213497a813b36b3a
haritsm <haritsmuhammad94@gmail.com>
2024-11-07 20:26:02 +0800

    test

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -13,9 +13,7 @@ export const ErrorPage: React.FC<{ statusCode: number }> = ({ statusCode }) => {
       <div className={styles.container}>
         <main className={styles.main}>
           <h1>Error Loading Page</h1>
-
           {statusCode && <p>Error code: {statusCode}</p>}
-
           <img src='/error.png' alt='Error' className={styles.errorImage} />
         </main>
       </div>
